dab499804c82abbdf2022bf76d8e9eb3d891ae6f
Eptagone <jheng1999@outlook.com>
2020-01-23 13:22:17 -0600

    Add a quiz poll sample

---
+++ src/Telegram.BotAPI.Samples/Poll Quiz 01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Telegram.BotAPI;
+using Telegram.BotAPI.Available_Methods;
+using Telegram.BotAPI.Getting_updates;
+
+namespace Poll_Quiz_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new BotClient("<bot token>");
+            while(true){
+                var updates = bot.GetUpdates();
+                if(updates.Length > 0){
+                    foreach(var update in updates){
+                        switch(update.Type){
+                            case UpdateType.Message:
+                                if(update.Message.Text.Contains("/quiz")){
+                                    bot.SendPoll(new SendPollArgs
+                                    {
+                                        Chat_id = update.Message.Chat.Id,
+                                        Reply_to_message_id = update.Message.Message_id,
+                                        Question = "¿2 + 5?",
+                                        Options = new string[]{
+                                            "6", "7", "5", "-4"
+                                        },
+                                        Type = "quiz",
+                                        Correct_option_id = 1
+                                    });
+                                }
+                                break;
+                        }
+                    }
+                    updates = bot.GetUpdates(offset: updates.Max(u => u.Update_id) + 1);
+                }
+            }
+        }
+    }
+}
