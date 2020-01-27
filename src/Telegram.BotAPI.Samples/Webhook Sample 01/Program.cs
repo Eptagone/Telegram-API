8657d555e9eee9890c73ecf37e95574be047aa4f
Eptagone <jheng1999@outlook.com>
2020-01-27 15:17:16 -0600

    update samples

--- src/Telegram.BotAPI.Samples/Webhook Sample 01/Program.cs
+++ src/Telegram.BotAPI.Samples/Webhook Sample 01/Program.cs
@@ -13,10 +13,10 @@ namespace Webhook_Sample_01
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-.ConfigureWebHostDefaults(webBuilder =>
-{
-    webBuilder.UseStartup<Startup>();
-});
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
         }
     }
 }
--- src/Telegram.BotAPI.Samples/Webhook Sample 01/TBot.cs
+++ src/Telegram.BotAPI.Samples/Webhook Sample 01/TBot.cs
@@ -4,6 +4,6 @@ namespace Webhook_Sample_01
 {
     public static class TBot
     {
-        public static readonly BotClient Bot = new BotClient("1005362332:AAF1LY_joAEA5CIXpvUbylKvgcY7ytRn0m4");
+        public static readonly BotClient Bot = new BotClient("<your bot token>");
     }
 }
